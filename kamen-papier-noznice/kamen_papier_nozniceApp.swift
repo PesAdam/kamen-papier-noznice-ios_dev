a4fb3bbfe24db9ac3d168e931eb2b713a27fd2de
PesAdam <pesadam15@gmail.com>
2022-01-31 21:00:10 +0100

    Initial Commit

---
+++ kamen-papier-noznice/kamen_papier_nozniceApp.swift
@@ -0,0 +1,17 @@
+//
+//  kamen_papier_nozniceApp.swift
+//  kamen-papier-noznice
+//
+//  Created by Bender on 31/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct kamen_papier_nozniceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
