b11ff0c95797e0cb71620fa16785c99606b79b90
PesAdam <pesadam15@gmail.com>
2022-02-02 16:12:05 +0100

    vytvorenie si 3 tlacitok, enumeracia a podobne veci

--- kamen-papier-noznice/ContentView.swift
+++ kamen-papier-noznice/ContentView.swift
@@ -9,29 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack{
-        
-            Image(systemName: "scissors")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 50)
+        HStack{
             
-            Image(systemName: "scissors")
+            CustomButton(choice: .kamen)
+            CustomButton(choice: .papier)
+            CustomButton(choice: .noznice)
+            
+            /*Image(systemName: "octagon.fill")
                 .resizable()
                 .scaledToFit()
                 .frame(width: 50)
        
-            Image(systemName: "scissors")
+            Image(systemName: "newspaper.fill")
                 .resizable()
                 .scaledToFit()
                 .frame(width: 50)
-        
+             */
         }
     }
 }
 
+//enumeracia
+enum Choice : String {
+    case kamen = "octagon.fill"
+    case noznice = "scissors"
+    case papier = "newspaper.fill"
+}
+
+//aby som sa neopakoval ako retard pouzil som extract subview
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CustomButton: View {
+    let choice: Choice
+    
+    var body: some View {
+        Button{
+            print(choice)
+        } label: {
+            Image(systemName: choice.rawValue )
+                .resizable()
+                .scaledToFit()
+                .frame(width: 50)
+                .foregroundColor(.black)
+        }
     }
 }
