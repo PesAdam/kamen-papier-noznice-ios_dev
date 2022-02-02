d9b240b31a6663657abd5a3cde990d05302a47f8
PesAdam <pesadam15@gmail.com>
2022-02-02 17:58:03 +0100

    animovanie

--- kamen-papier-noznice/ContentView.swift
+++ kamen-papier-noznice/ContentView.swift
@@ -11,35 +11,77 @@ struct ContentView: View {
     
     @State var myChoice: Choice = .kamen
     @State var opponentsChoice: Choice = .papier
+    @State var opponentsChoiceIsShow: Bool = false
+    @State var showingresult: Bool = false
     
+    var randomChoice: Choice {
+        let choices: [Choice] = [.kamen, .papier, .noznice]
+        
+        let randomIndex = Int.random(in: 0...2)
+        
+        return choices[randomIndex]
+    }
     
     var body: some View {
         GeometryReader { geo in
-            VStack {
-                ZStack {
-                    Rectangle()
-                        .frame(height: geo.size.height / 2)
-                    Image(systemName: opponentsChoice.rawValue)
+            ZStack {
+                VStack {
+                    ZStack {
+                        Rectangle()
+                            .frame(height: geo.size.height / 2)
+                        
+                        if opponentsChoiceIsShow {
+                            Image(systemName: opponentsChoice.rawValue)
+                                .resizable()
+                                .foregroundColor(.white)
+                                .scaledToFit()
+                                .frame(height: geo.size.height / 3)
+                                .padding()
+                        } else{
+                            Button{
+                                opponentsChoice = randomChoice
+                                opponentsChoiceIsShow = true
+                                withAnimation{
+                                    showingresult = true 
+                                }
+                                
+                            } label: {
+                                Text("PLAY")
+                                    .font(.system(size: 75, weight: .bold))
+                                    .foregroundColor(.white)
+                            }
+                        }
+                    }
+                    
+                    Image(systemName: myChoice.rawValue)
                         .resizable()
-                        .foregroundColor(.white)
                         .scaledToFit()
                         .frame(height: geo.size.height / 3)
                         .padding()
-                }
-                
-                Image(systemName: myChoice.rawValue)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(height: geo.size.height / 3)
-                    .padding()
-                
-                HStack(spacing: 40){
                     
-                    CustomButton(choice: .kamen, myChoice: $myChoice)
-                    CustomButton(choice: .papier, myChoice: $myChoice)
-                    CustomButton(choice: .noznice, myChoice: $myChoice)
-                    
+                    HStack(spacing: 40){
+                        
+                        CustomButton(choice: .kamen, myChoice: $myChoice)
+                        CustomButton(choice: .papier, myChoice: $myChoice)
+                        CustomButton(choice: .noznice, myChoice: $myChoice)
+                        
+                    }
                 }
+            }
+
+                
+                Text("WIN")
+                    .padding(50)
+                    .font(.system(size: 75, weight: .bold))
+                    .foregroundColor(.blue)
+                    .background(Color.black)
+                    .border(Color.white, width: 3)
+                    .cornerRadius(3, antialiased: true)
+                    .offset(x: showingresult ? 0 : -400)
+                    .onTapGesture {
+                        opponentsChoiceIsShow = false
+                        showingresult = false
+
             }
         } .edgesIgnoringSafeArea(.all)
     }
